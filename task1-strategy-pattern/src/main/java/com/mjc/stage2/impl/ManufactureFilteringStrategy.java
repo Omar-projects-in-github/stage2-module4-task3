e786bca553cfd27cb4775284a7d799dd139082d6
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-12-28 22:58:20 +0600

    Strategy pattern was implemented

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/ManufactureFilteringStrategy.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/ManufactureFilteringStrategy.java
@@ -1,11 +1,14 @@
 package com.mjc.stage2.impl;
 
 
+import com.mjc.stage2.Product;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class ManufactureFilteringStrategy {
     private String manufacture;
 
-    // Write your code here!
+    public boolean filter(Product product) {
+        return manufacture.toLowerCase().equals(product.getManufacture().toLowerCase());
+    }
 }
--- task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/MaxPriceFilteringStrategy.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/impl/MaxPriceFilteringStrategy.java
@@ -1,11 +1,14 @@
 package com.mjc.stage2.impl;
 
 
+import com.mjc.stage2.Product;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class MaxPriceFilteringStrategy {
     private double maxPrice;
 
-    // Write your code here!
+    public boolean filter(Product product) {
+        return maxPrice == product.getPrice();
+    }
 }

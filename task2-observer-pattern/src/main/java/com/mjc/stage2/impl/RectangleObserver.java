e6f1733b1b6a47b24b215fe13c4419cf3afddf99
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-12-29 19:53:50 +0600

    Observer pattern was implemented

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,15 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
-    // Write your code here!
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.event.RectangleEvent;
+
+public class RectangleObserver implements Observer {
+    public void handleEvent(RectangleEvent event) {
+        Rectangle rectangle = event.getSource();
+        double sideA = rectangle.getSideA();
+        double sideB = rectangle.getSideB();
+        System.out.println("Square is " + sideA * sideB);
+        System.out.println("Perimeter is " + 2 * (sideA + sideB));
+    }
 }

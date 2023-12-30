639f91f47c8fb108506ba014b80a643af6e5761e
Omar-projects-in-github <polat.omar2002@gmail.com>
2023-12-30 21:33:45 +0600

    Inside the RectangleObserver I added RectangleValues and RectangleWarehouse changes using getInstance

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -2,14 +2,20 @@ package com.mjc.stage2.impl;
 
 import com.mjc.stage2.Observer;
 import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
 import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
 
 public class RectangleObserver implements Observer {
     public void handleEvent(RectangleEvent event) {
         Rectangle rectangle = event.getSource();
         double sideA = rectangle.getSideA();
         double sideB = rectangle.getSideB();
-        System.out.println("Square is " + sideA * sideB);
-        System.out.println("Perimeter is " + 2 * (sideA + sideB));
+
+        double square = sideA * sideB;
+        double perimeter = 2 * (sideA + sideB);
+
+        RectangleValues values = new RectangleValues(square, perimeter);
+        RectangleWarehouse.getInstance().put(rectangle.getId(), values);
     }
 }
